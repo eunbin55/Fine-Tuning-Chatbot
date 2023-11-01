4be78da30c1629997102f29d0785bdc899548517
eunbin55 <eunbin620@naver.com>
2023-11-02 01:50:31 +0900

    first commit

---
+++ backend/fine_tuning_chatbot/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class BoardPost (models.Model):
+    title= models.CharField(max_length=200)
+    content=models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self) :
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return self.title
---
+++ backend/fine_tuning_chatbot/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from .models import Book
+
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'fine_tuning_chatbot/template/create_book.html', {'form': form})
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'fine_tuning_chatbot/template/book_list.html', {'books': books})
