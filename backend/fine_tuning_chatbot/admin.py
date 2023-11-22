71ed5df709b07fb5f79676c357a6c01d6f90b35e
eunbin55 <eunbin620@naver.com>
2023-11-22 23:48:51 +0900

    [ADD] Model

--- backend/fine_tuning_chatbot/admin.py
+++ backend/fine_tuning_chatbot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import FineTunedModel, TrainingData
 
-# Register your models here.
+@admin.register(FineTunedModel)
+class FineTunedModelAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'base_model')
+    search_fields = ('model_name', 'base_model')
+
+@admin.register(TrainingData)
+class TrainingDataAdmin(admin.ModelAdmin):
+    list_display = ('prompt', 'completion', 'fine_tuned_model')
+    search_fields = ('model_name', 'base_model')
+    list_filter = ('fine_tuned_model',)
+
+    
+
+    
